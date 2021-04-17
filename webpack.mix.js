53b0f36f9e9bc160005bde05a0e5d317bb9d9be6
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-17 16:28:10 +0200

    lavoro su jumbotron

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,7 @@
 let mix = require('laravel-mix');
 
-mix.sass('src/scss/master.scss', 'dist/css/')
+mix.sass('src/scss/master.scss', 'dist/css/');
+
+mix.sass('src/scss/master.scss', 'dist/img/').options({
+    processCssUrls: false
+});
